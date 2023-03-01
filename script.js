19d7c246580366d6b2b30416d0124d196fa5a84c
ReMelon <116052436+ReMelon@users.noreply.github.com>
2023-03-01 18:22:22 +0900

    final front banner

--- script.js
+++ script.js
@@ -1,29 +1,87 @@
-window.onload = function() {
-    const section = document.querySelector('.section');
-    const imageContainer = document.querySelector('.image-container');
-    const images = Array.from(imageContainer.children);
+const images = document.querySelectorAll('.image');
+const blackSection = document.querySelector('.black-section');
+const height = blackSection.offsetHeight;
+const width = blackSection.offsetWidth;
 
-    function setPosition() {
-        images.forEach((image) => {
-            const rect = image.getBoundingClientRect();
-            const x = Math.random() * (section.offsetWidth - rect.width);
-            const y = Math.random() * (section.offsetHeight - rect.height);
-            image.style.left = `${x}px`;
-            image.style.top = `${y}px`;
-        });
-    }
+images.forEach(image => {
+    const x = Math.floor(Math.random() * (width - image.width));
+    const y = Math.floor(Math.random() * (height - image.height));
+    image.style.top = `${y}px`;
+    image.style.left = `${x}px`;
+    fadeIn(image);
+});
 
-    images.forEach((image) => {
-        image.style.opacity = 1;
-    });
-
-    setInterval(() => {
-        images.forEach((image) => {
-            image.style.opacity = 0;
+function fadeIn(element) {
+    element.style.opacity = 0;
+    let opacity = 0;
+    const intervalId = setInterval(() => {
+        opacity += 0.05;
+        element.style.opacity = opacity;
+        if (opacity >= 1) {
+            clearInterval(intervalId);
             setTimeout(() => {
-                setPosition();
-                image.style.opacity = 1;
-            }, 4000);
-        });
-    }, 8000);
+                fadeOut(element);
+            }, 2000);
+        }
+    }, 50);
+}
+
+function fadeOut(element) {
+    let opacity = 1;
+    const intervalId = setInterval(() => {
+        opacity -= 0.05;
+        element.style.opacity = opacity;
+        if (opacity <= 0) {
+            clearInterval(intervalId);
+            const x = Math.floor(Math.random() * (width - element.width));
+            const y = Math.floor(Math.random() * (height - element.height));
+            element.style.top = `${y}px`;
+            element.style.left = `${x}px`;
+            fadeIn(element);
+        }
+    }, 50);
+}
+
+let index = 0,
+    interval = 1000;
+
+const rand = (min, max) =>
+    Math.floor(Math.random() * (max - min + 1)) + min;
+
+const animate = star => {
+    star.style.setProperty("--star-left", `${rand(-10, 100)}%`);
+    star.style.setProperty("--star-top", `${rand(-40, 80)}%`);
+
+    star.style.animation = "none";
+    star.offsetHeight;
+    star.style.animation = "";
+}
+
+
+
+/* -- ↓↓↓ If you want the sparkle effect to only occur on hover, replace lines 16 and on with this code ↓↓↓ -- */
+
+let timeouts = [],
+    intervals = [];
+
+const magic = document.querySelector(".magic");
+
+magic.onmouseenter = () => {
+    let index = 1;
+
+    for (const star of document.getElementsByClassName("magic-star")) {
+        timeouts.push(setTimeout(() => {
+            animate(star);
+
+            intervals.push(setInterval(() => animate(star), 1000));
+        }, index++ * 300));
+    };
+}
+
+magic.onmouseleave = onMouseLeave = () => {
+    for (const t of timeouts) clearTimeout(t);
+    for (const i of intervals) clearInterval(i);
+
+    timeouts = [];
+    intervals = [];
 }
